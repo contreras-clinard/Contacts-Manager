2efafcf70455c51444627d93ad1cb28e43907571
Alexandra Contreras <alexandra.contreras0903@gmail.com>
2019-08-08 13:35:43 -0500

    fixed search contact function

--- src/Contacts.java
+++ src/Contacts.java
@@ -1,3 +1,4 @@
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -5,23 +6,32 @@ import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
 
 public class Contacts {
     private String firstName;
     private String lastName;
     private String phoneNumber;
 
+    File file = new File("/Users/Alexandra/IdeaProjects/Contacts-Manager/data/contacts.txt");
+    Scanner scanner = new Scanner(file);
+    Path contactsPath = Paths.get("data", "contacts.txt");
+    List<String> list = Files.readAllLines(contactsPath);
+
+
+
 
 //    constructor
 
-    public Contacts(String firstName, String lastName, String phoneNumber){
+    public Contacts(String firstName, String lastName, String phoneNumber) throws IOException {
         this.firstName=firstName;
         this.lastName = lastName;
         this.phoneNumber=phoneNumber;
     }
 
-    public void setPhoneNumber(String num){
-        this.phoneNumber=num;
+    public void setPhoneNumber(String phoneNumber){
+        this.phoneNumber= phoneNumber;
     }
 
     public void setName(String firstName, String lastName){
@@ -40,7 +50,6 @@ public class Contacts {
 
 
     //-- TO ADD A CONTACT;
-    Path contactsPath = Paths.get("data", "contacts.txt");
 
     public void addContact(String firstName, String lastName, String phoneNumber) throws IOException {
         String concat=this.firstName+" "+this.lastName+" "+this.phoneNumber;
@@ -49,8 +58,37 @@ public class Contacts {
         );
     }
 
+//    Search for Contact
+    public void searchContact(String input) {
+//
+        for (int i = 0; i < list.size() - 1; i++) {
+            if (list.get(i).toLowerCase().contains(input)) {
+                System.out.println(list.get(i));
+//
+            }
+        }
+    }
+//Remove Contact
+//
+        public void removeContact(String input) throws IOException {
+        List<String> removeList= Files.readAllLines(contactsPath);
+       for(String line : removeList){
+           if (!line.contains(input)){
+               list.add(line);
+           }
+       }
+          Files.write(Paths.get(String.valueOf(file)),list);
+//
 
 
+
+
+
+
+
+//            list=removeList;
+//            Files.write(contactsPath,list);
+        }
 
 }
 
--- src/ContactsTest.java
+++ src/ContactsTest.java
@@ -1,6 +1,7 @@
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.StandardOpenOption;
+import java.sql.SQLOutput;
 import java.util.Scanner;
 import java.io.FileNotFoundException;
 import java.util.List;
@@ -11,7 +12,10 @@ import java.nio.file.Files;
 import java.util.ArrayList;
 //import java.util.
 
-public class ContactsTest {
+public class ContactsTest extends Contacts{
+    public ContactsTest(String firstName, String lastName, String phoneNumber) throws IOException {
+        super(firstName, lastName, phoneNumber);
+    }
 
 
 //    method to addContacts
@@ -25,19 +29,25 @@ public class ContactsTest {
     public static void main(String[] args) throws IOException {
 
 //        variables
-        File file = new File("/Users/brandi/IdeaProjects/Contacts-Manager/data/contacts.txt");
+        File file = new File("/Users/Alexandra/IdeaProjects/Contacts-Manager/data/contacts.txt");
         Scanner scanner = new Scanner(file);
         Scanner scan = new Scanner(System.in);
         Path contactsPath = Paths.get("data", "contacts.txt");
 
 
         //        Prints out List
-        ArrayList<String> list = new ArrayList<String>();
-        while (scanner.hasNextLine()) {
-            list.add(scanner.nextLine());
-            System.out.println();
-        }
-        System.out.println(list);
+////        ArrayList<String> list = new ArrayList<String>();
+//        while (scanner.hasNextLine()) {
+//            list.add(scanner.nextLine());
+//            System.out.println();
+//        }
+//        System.out.println(list);
+            List<String> list = Files.readAllLines(contactsPath);
+            for (int i = 0; i < list.size(); i += 1) {
+                System.out.println((i + 1) + " : " + list.get(i));
+            }
+
+
 
 
 //        System.out.println(list);
@@ -60,30 +70,48 @@ public class ContactsTest {
         Contacts contact = new Contacts(inputFirst, inputLast, inputNum);
         contact.addContact(inputFirst, inputLast, inputNum);
 
-        //--displays updated list;
-//        ArrayList<String> list = new ArrayList<String>();
-        list = new ArrayList<String>();
-        while (scanner.hasNextLine()) {
-            list.add(scanner.nextLine());
-            System.out.println();
+        list = Files.readAllLines(contactsPath);
+        for (int i = 0; i < list.size(); i += 1) {
+            System.out.println((i + 1) + " : " + list.get(i));
         }
-        System.out.println();
+
+                                                                          //--displays updated list;
+//        ArrayList<String> list = new ArrayList<String>();
+//        list = new ArrayList<String>();
+//        while (scanner.hasNextLine()) {
+//            list.add(scanner.nextLine());
+//            System.out.println();
+//
+//        }
+//        System.out.println();
+
+//        for (String line : list) {
+//            System.out.println(line);
+//        }
+
+
+
+
 
 
 //
-//
-////        Contact Search
-//        String findContact = list.get(list.indexOf("Allison"));
-//
+
+//        Contact Search
+//        String findContact = contact.searchContact("alexandra");
+
 //        System.out.println(findContact);
-//
+        contact.searchContact("sifuentes");
+     contact.searchContact("contreras");
+
+       
+
 ////
 //////        REMOVE CONTACTS
-////        List<String> contactList = contactList;
+//        List<String> contactList = contactList;
 //            list.remove(list.indexOf("userInput"));
 //            System.out.println(list);
 //            Files.write(contactsPath, list);
-//
+////
 
 
 
